aa5738fe32d00e6f3d16ed930f38058092891146
fioMei <muhammadfathurrahman012@gmail.com>
2022-11-29 09:21:54 +0700

    menambahkan container hijau

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,21 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(title: Text("Latihan Container"),),
-      body: Container(color: Colors.red,),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Container"),
+        ),
+        body: Container(
+          color: Colors.red,
+          margin: EdgeInsets.all(5),
+          padding: EdgeInsets.all(7),
+          child: Container(
+            color: Colors.green,
+          ),
+        ),
       ),
     );
   }
